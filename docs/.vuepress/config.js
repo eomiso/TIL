e1be1038b20e5ad1aac45d09ca3d2313d8d74eca
Uiseop Eom <ytrtef@gmail.com>
2019-10-30 21:08:28 +0900

    add navbar
    [Why]
    *

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,38 @@
 // last modified : 2019.10.28
 // author : eomiso
 
+var CONST = require("./const");
+
 
 module.exports = {
   title: "Eomiso's TIL(Today I Learned)",
   description: 'Write what you have learned today',
-  base: "/TIL/"
+  base: "/TIL/",
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/logo.png'
+    }]
+  ],
+  themeConfig: {
+    sidebar: [
+      {
+        title: '1. Python',
+        children: CONST.PythonList
+      },{
+        title: '2. Git',
+        children: CONST.GitList
+      }
+    ],
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/eomiso'
+      },{
+        text: 'Blog',
+        link: 'https://eomiso.github.io'
+      }
+    ]
+  }
 }
 
